10dd2bb802b556be15654156ce47ff4edd302609
LuvanTabares <luvantabares@gmail.com>
2023-02-26 23:22:20 -0500

    r

---
+++ modulo2 DocstringDoctest/dModuloDoctest/main.py
@@ -0,0 +1,22 @@
+# Docstring
+# __doc__
+
+def palindromo(sentence: str) -> bool:
+    """Permite conocer si un string es un palindromo.
+
+    Args:
+        sentence (str): String a evaluar.
+
+    Returns:
+        bool: True o False.
+
+    Examples:
+
+    >>> palindromo('Anita lava la tina')
+    True
+    """
+    sentence = sentence.lower().replace(' ', '')
+    return sentence == sentence[::-1]
+
+
+# python -m doctest main.py -v
